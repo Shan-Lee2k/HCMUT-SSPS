b0a32c313d8aa9b4bad3488a9017cbf73313e50d
Lee Shan <120365693+Shan-Lee2k@users.noreply.github.com>
2023-12-02 12:08:39 +0700

    UI_flow
    For User and SPSO

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, flash, redirect, url_for, request
+from flask import Flask, render_template, flash, redirect, url_for, request,session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
@@ -35,21 +35,46 @@ class Post(db.Model):
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
 
-posts = [
-    {
-        'author' : 'Shan',
-        'title' : "How to read?",
-        'price' : 21
-    },
-    {
-        'author' : 'Shan',
-        'title' : "How to sing",
-        'price' : 50
-    }
-]
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
+def init():
+    if request.method == "POST":
+        user_type = request.form.get('user_type')
+        # Storing userType in the session
+        session['user_type'] = user_type
+
+        # Redirect to appropriate page based on user type
+        if user_type == 'user':
+            return redirect(url_for('login'))  # Redirect to user login
+        elif user_type == 'spso':
+            return redirect(url_for('login'))  # Redirect to SPSO login
+    return render_template("init.html")
+@app.route('/home')
 def home():
-    return render_template("hello.html", posts = posts)
+    if "user_type" in session:
+        user_type = session['user_type']
+
+        # Redirect to appropriate page based on user type
+        if user_type == 'user':
+            return render_template("user_dashboard.html") 
+        elif user_type == 'spso':
+            return render_template("spso_dashboard.html")
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("You have been logged in", 'success')
+        if "user_type" in session:
+            user_type = session.get('user_type')
+            if user_type == 'user':
+                return render_template("user_dashboard.html")  
+            elif user_type == 'spso':
+                return render_template("spso_dashboard.html")  
+    else:
+        #flash("Login unsuccessfully! Please check email and password again!")
+        return render_template('login.html', form=form)
+
+
 @app.route('/print', methods = ['GET', 'POST'])
 def print():
     if request.method == 'POST':
@@ -57,22 +82,12 @@ def print():
       f.save(secure_filename(f.filename))
       return 'file uploaded successfully'
     render_template('print.html')
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}", 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title = 'Register', form = form)
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash("You have been logged in", 'success')
-        return redirect(url_for('home'))
-    else:
-        flash("Login unsuccessfully! Please check email and password again!")
-    return render_template('login.html', title = 'Login', form = form)
+    
+# Log out
+@app.route('/logout')
+def logout():
+    return render_template("init.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
